de67623bfbd2b58e53a3d872f69b4709bdfd361d
isislvx <isislvx@yahoo.com>
2014-10-22 22:12:06 -0400

    Finish Toy App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = 'ce998ce25a15af12a7de0ba7d40e9031fd493b7ca383fd5fb5a43aebeba9995ed46d02221553d2f79e1cc36c6a045055b5216335cec97bc987160884450e91de'
